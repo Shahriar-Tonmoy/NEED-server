e44799ba3b457e2e1330b601de47708336b1cca6
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-02 02:39:42 +0600

    vercel config added

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-//Need-master
-//cc6vUTXcOti1eoUz
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res)=>{
-    res.send('server is ready');
+    res.send('NEED server is ready');
 })
 
 app.listen(port, ()=>{
@@ -78,11 +76,11 @@ async function run() {
         const result = await cursor.toArray();
         res.send(result);
       })
+
     app.get('/bidJobs', async(req, res) =>{
         
         const cursor = bidJobsCollection.find();
         const result = await cursor.toArray();
-        sort( { age : -1, posts: 1 } )
         res.send(result);
     })
     //get one data
@@ -159,8 +157,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     //await client.close();
